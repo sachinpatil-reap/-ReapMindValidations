f040a3ec509d9f4d0d361969b84ad3ff2fc13baf
Aniket patil <aniket.patil.rm@synergygfs.com>
2024-06-04 15:18:42 +0530

    Initial Commit

---
+++ ReapMindValidations/ReapMindValidations.h
@@ -0,0 +1,18 @@
+//
+//  ReapMindValidations.h
+//  ReapMindValidations
+//
+//  Created by Sachin Patil on 04/06/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ReapMindValidations.
+FOUNDATION_EXPORT double ReapMindValidationsVersionNumber;
+
+//! Project version string for ReapMindValidations.
+FOUNDATION_EXPORT const unsigned char ReapMindValidationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReapMindValidations/PublicHeader.h>
+
+
